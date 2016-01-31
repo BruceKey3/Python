0ff2694455f9e3e2fe568d02e7ebbf6ca79edc69
BruceKey3 <mrbrucekey3@gmail.com>
2016-01-31 14:22:08 +0000

    Changed to take argument from command line or default to 5.£

--- pascal.py
+++ pascal.py
@@ -1,11 +1,11 @@
+import sys
+
 class Solution(object):
     def pascal(self, list, numRows, level):
         if(level > numRows):
             return
         newList = [1]
         if level > 1:
-            print(len(list))
-            print(level)
             prevLevel= list[level-2]
             for i  in range(1, level - 1):
                 res = prevLevel[i-1] + prevLevel[i]
@@ -14,15 +14,15 @@ class Solution(object):
         list.append(newList)
         self.pascal(list, numRows, (level+1))
     
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
+    def generate(self, numRows=5):
         returnList = []
         self.pascal(returnList,numRows,1)
         return returnList
 
 solution = Solution()
-list = solution.generate(5)
+if(len(sys.argv) == 2):
+    list = solution.generate(int(sys.argv[1]))
+else:
+    list = solution.generate()
+    
 print(list)
